45df53d5bca7edafec8558724944462b216e9056
Dan Hudlow <dhudlow@us.ibm.com>
2023-07-02 13:25:22 -0500

    Test web server

---
+++ node-express/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (request, response) => {
+  let parameters = {};
+
+  for (const param in request.query) {
+    let value = request.query[param];
+
+    if (param === 'cast_to_boolean') {
+      value = Boolean(value);
+    } else if (param === 'cast_to_integer') {
+      value = parseInt(value);
+    } else if (param === 'cast_to_float') {
+      value = parseFloat(value);
+    } else if (param === 'cast_to_date_time') {
+      let date = new Date();
+      date.setTime(Date.parse(value));
+      value = date.toJSON();
+    }
+
+    parameters[param] = JSON.stringify(value);
+  }
+
+  response.send(JSON.stringify(parameters) + "\n");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
